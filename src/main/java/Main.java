a7c47db73e4099100d272cf9431f8575253d0441
zarhouni88 <fatmtalzhraalzrhwny@gmail.com>
2024-10-22 21:07:38 +0100

    Change the maven.compiler.source to 17 along with the java.version, and the yaml configurations

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,26 +9,26 @@ public class Main {
 
     // Uncomment this block to pass the first stage
     // 
-    // ServerSocket serverSocket = null;
-    // Socket clientSocket = null;
-    // int port = 9092;
-    // try {
-    //   serverSocket = new ServerSocket(port);
-    //   // Since the tester restarts your program quite often, setting SO_REUSEADDR
-    //   // ensures that we don't run into 'Address already in use' errors
-    //   serverSocket.setReuseAddress(true);
-    //   // Wait for connection from client.
-    //   clientSocket = serverSocket.accept();
-    // } catch (IOException e) {
-    //   System.out.println("IOException: " + e.getMessage());
-    // } finally {
-    //   try {
-    //     if (clientSocket != null) {
-    //       clientSocket.close();
-    //     }
-    //   } catch (IOException e) {
-    //     System.out.println("IOException: " + e.getMessage());
-    //   }
-    // }
+     ServerSocket serverSocket = null;
+     Socket clientSocket = null;
+     int port = 9092;
+     try {
+       serverSocket = new ServerSocket(port);
+       // Since the tester restarts your program quite often, setting SO_REUSEADDR
+       // ensures that we don't run into 'Address already in use' errors
+       serverSocket.setReuseAddress(true);
+       // Wait for connection from client.
+       clientSocket = serverSocket.accept();
+     } catch (IOException e) {
+       System.out.println("IOException: " + e.getMessage());
+     } finally {
+       try {
+         if (clientSocket != null) {
+           clientSocket.close();
+         }
+       } catch (IOException e) {
+         System.out.println("IOException: " + e.getMessage());
+       }
+     }
   }
 }
