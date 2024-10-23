d7d17f9423b816e9e6f56ba02bfab9d40c305849
zarhouni88 <fatmtalzhraalzrhwny@gmail.com>
2024-10-23 01:57:32 +0100

    testing that the message length is fetched correctly : debug mode is on

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -26,6 +26,7 @@ public class Main {
 
          clientSocket = serverSocket.accept();
          while(true){
+             System.out.println("New Loop : ");
              rawRequest = clientSocket.getInputStream() ;
              rawResponse = clientSocket.getOutputStream() ;
              handleRequest(rawRequest , rawResponse);
