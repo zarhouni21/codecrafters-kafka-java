0ec5ae88b284704437cf48c4dddf81251467db2a
zarhouni88 <fatmtalzhraalzrhwny@gmail.com>
2024-10-25 15:49:46 +0100

    testing that the message length is fetched correctly : debug mode is on

--- src/main/java/Server/Server.java
+++ src/main/java/Server/Server.java
@@ -27,15 +27,17 @@ public class Server extends Thread {
         try{
             Socket client = server.accept() ;
             System.out.println("SERVER: new connection have been made : ===========================");
-            while(client!=null && client.getInputStream()!=null){ // to handle multiple requests from the client
-                Request request = new Request() ;
-                request.readRequestFromStream(client.getInputStream());
-                System.out.println("NEW REQUEST : request's correlation Id is:" + request.getHeader().getCorrelationId());
-                Response response = Response.fromRequest(request) ;
-                int responseLength = response.encodeResponse().length ;
-                client.getOutputStream().write(PrimitiveOperations.fromIntToByteArray(responseLength));
-                client.getOutputStream().write(response.encodeResponse());
-                client.getOutputStream().flush();
+            while(client!=null){ // to handle multiple requests from the client
+                if(client.getInputStream().available()!=0){
+                    Request request = new Request() ;
+                    request.readRequestFromStream(client.getInputStream());
+                    System.out.println("NEW REQUEST : request's correlation Id is:" + request.getHeader().getCorrelationId());
+                    Response response = Response.fromRequest(request) ;
+                    int responseLength = response.encodeResponse().length ;
+                    client.getOutputStream().write(PrimitiveOperations.fromIntToByteArray(responseLength));
+                    client.getOutputStream().write(response.encodeResponse());
+                    client.getOutputStream().flush();
+                }
             }
         }catch (IOException e){
             System.out.println("SERVER, error : " + e.toString());
